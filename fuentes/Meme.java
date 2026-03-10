0e2a09e208922f14f116281eae892628d0d9e460
jgutierrezr02 <jgutierrezr02@educarex.es>
2026-03-10 21:07:57 +0100

    ArchivosClave

---
+++ fuentes/Meme.java
@@ -0,0 +1,22 @@
+public class Meme {
+
+    private int id;
+    private String texto;
+
+    public Meme(int id, String texto){
+        this.id = id;
+        this.texto = texto;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getTexto(){
+        return texto;
+    }
+
+    public String toString(){
+        return id + " - " + texto;
+    }
+}
